86ac052d64d264dfe45bfd82d614365b62b2915c
Alex <night_guard@abv.bg>
2016-12-08 23:16:24 +0200

    Rejected challenge test case.

--- hamster-parent/hamster-web/src/test/java/org/hamster/test/AbstractHamsterTest.java
+++ hamster-parent/hamster-web/src/test/java/org/hamster/test/AbstractHamsterTest.java
@@ -20,9 +20,13 @@ import org.hamster.service.UserRelationServiceImpl;
 import org.hamster.service.UserServiceImpl;
 import org.hamster.service.VoteServiceImpl;
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/test/resources/test-context.xml"})
 public abstract class AbstractHamsterTest
 {
     @Autowired
